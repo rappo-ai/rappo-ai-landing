224cd3bfe2856c10ef1254c033a0621519ad7fa4
Renzil D'Souza <renzil@gmail.com>
2022-03-23 00:25:43 +0530

    Adding meta tags for og:image twitter:image

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,13 +15,24 @@ import SecurePayment from 'sections/secure-payment';
 import Package from 'sections/package';
 import Faq from 'sections/faq';
 import Register from 'sections/register';
+import getConfig from 'next/config'
 
 export default function IndexPage() {
+  const meta = [
+    {
+      name: `og:image`,
+      content: `/static/images/landing_graphic-6177d4be038b1a8f4c0ae87f65938014.png`,
+    },
+    {
+      name: `twitter:image`,
+      content: `/static/images/landing_graphic-6177d4be038b1a8f4c0ae87f65938014.png`,
+    },
+  ];
   return (
     <ThemeProvider theme={theme}>
       <StickyProvider>
         <Layout>
-          <SEO title="Rappo - Live Chat Tool" />
+          <SEO title="Rappo - Live Chat Tool" description="Instantly Connect with Your Customers" meta={meta}/>
           <Banner />
           <Register/>
           {/* <KeyFeature />
